f047e6fee39e1175d68703422b75efaf6831a9b6
GUILLEMETTE Matthieu <mathieu.guillemette@caissedesdepots.fr>
2015-12-21 11:33:13 +0100

    feature-11-align-elink-desktop : modification prise en compte

--- www/js/bibli/el1-controller-view.js
+++ www/js/bibli/el1-controller-view.js
@@ -1,155 +1,170 @@
-(function(){
+(function () {
 
-    angular
-        .module('el1.bibli')
-        .controller('bibliController', [
-            '$log', '$scope', '$rootScope', '$state',
-            'LiensService', 'GestionService', 'UsersManager', 'SessionStorage', 'USERFIREBASEPROFILEKEY',
-            'liensNonLus', 'liensLus', 'allMyCercles', 'allCategories',
-            '$ionicPopup',
-            '$stateParams', '$timeout', 'ionicMaterialInk', 'ionicMaterialMotion',
-            BibliController
-            ]);
+  angular
+    .module('el1.bibli')
+    .controller('bibliController', [
+      '$log', '$scope', '$rootScope', '$state',
+      'LiensService', 'GestionService', 'UsersManager', 'SessionStorage', 'USERFIREBASEPROFILEKEY',
+      'liensNonLus', 'liensLus', 'allMyCercles', 'allCategories',
+      '$ionicPopup',
+      '$stateParams', '$timeout', 'ionicMaterialInk', 'ionicMaterialMotion',
+      BibliController
+    ]);
 
 
-    /**
-     */
-    function BibliController($log, $scope, $rootScope, $state, LiensService, GestionService, UsersManager, SessionStorage, USERFIREBASEPROFILEKEY, liensNonLus, liensLus, allMyCercles, allCategories, $ionicPopup, $stateParams, $timeout, ionicMaterialInk, ionicMaterialMotion ) {
-      $scope.$parent.showHeader();
-      $scope.$parent.clearFabs();
-      $scope.isExpanded = true;
-      $scope.$parent.setExpanded(true);
-      $scope.$parent.setHeaderFab('right');
+  /**
+   */
+  function BibliController($log, $scope, $rootScope, $state, LiensService, GestionService, UsersManager, SessionStorage, USERFIREBASEPROFILEKEY, liensNonLus, liensLus, allMyCercles, allCategories, $ionicPopup, $stateParams, $timeout, ionicMaterialInk, ionicMaterialMotion) {
+    $scope.$parent.showHeader();
+    $scope.$parent.clearFabs();
+    $scope.isExpanded = true;
+    $scope.$parent.setExpanded(true);
+    $scope.$parent.setHeaderFab('right');
 
-      $scope.replayAnimation = function() {
-        $timeout(function () {
-          ionicMaterialMotion.fadeSlideIn({
-            selector: '.animate-fade-slide-in .item'
-          });
-        }, 200);
-      };
+    $scope.replayAnimation = function () {
+      $timeout(function () {
+        ionicMaterialMotion.fadeSlideIn({
+          selector: '.animate-fade-slide-in .item'
+        });
+      }, 200);
+    };
 
-      $scope.replayAnimation();
+    $scope.replayAnimation();
 
-      // Activate ink for controller
-      ionicMaterialInk.displayEffect();
+    // Activate ink for controller
+    ionicMaterialInk.displayEffect();
 
-      //liens : liens non lus ou biblio selon le cas
+    //liens : liens non lus ou biblio selon le cas
 
+    if ($state.current.name === 'app.bibli-nonLu') {
+      $scope.liens = liensNonLus;
+    } else {
+      $scope.liens = liensLus;
+    }
+
+    $scope.doRefresh = function () {
       if ($state.current.name === 'app.bibli-nonLu') {
-          $scope.liens = liensNonLus;
+        LiensService.findNotReadLinksByUser(SessionStorage.get(USERFIREBASEPROFILEKEY).uid)
+          .then(function (liensNonLusRefresh) {
+            $scope.liens = liensNonLusRefresh;
+          })
+          .finally(function () {
+            // Stop the ion-refresher from spinning
+            $scope.$broadcast('scroll.refreshComplete');
+            $scope.replayAnimation();
+          });
       } else {
-          $scope.liens = liensLus;
+        LiensService.findReadLinksByUser(SessionStorage.get(USERFIREBASEPROFILEKEY).uid)
+          .then(function (liensLusRefresh) {
+            $scope.liens = liensLusRefresh;
+          })
+          .finally(function () {
+            // Stop the ion-refresher from spinning
+            $scope.$broadcast('scroll.refreshComplete');
+            $scope.replayAnimation();
+          });
+      }
+      UsersManager.findCerclesByUser(SessionStorage.get(USERFIREBASEPROFILEKEY).uid)
+        .then(function (cerclesRefresh) {
+          allMyCercles = cerclesRefresh;
+        });
+    };
+
+    //TODO utilisation cordova-plugin-inappbrowser
+    $scope.showURL = function (lien) {
+      window.open(lien.url, '_system', 'location=yes');
+    };
+
+    $scope.canShare = function () {
+      return allMyCercles && allMyCercles[0];
+    };
+
+    $scope.deleteLink = function (lien) {
+      // $scope.liens est synchronisé avec la base
+      $scope.liens.$remove(lien);
+    };
+
+    $scope.moveTo = function (lien) {
+
+      // on conserve l'id original du lien dans keyOri
+      if (!lien.keyOri) {
+        lien.keyOri = lien.$id;
       }
 
-      $scope.doRefresh = function() {
-        if ($state.current.name === 'app.bibli-nonLu') {
-          LiensService.findNotReadLinksByUser(SessionStorage.get(USERFIREBASEPROFILEKEY).uid)
-            .then(function(liensNonLusRefresh) {
-                $scope.liens = liensNonLusRefresh;
-            })
-            .finally(function() {
-              // Stop the ion-refresher from spinning
-              $scope.$broadcast('scroll.refreshComplete');
-              $scope.replayAnimation();
-            });
-        } else {
-          LiensService.findReadLinksByUser(SessionStorage.get(USERFIREBASEPROFILEKEY).uid)
-            .then(function(liensLusRefresh) {
-              $scope.liens = liensLusRefresh;
-            })
-            .finally(function() {
-              // Stop the ion-refresher from spinning
-              $scope.$broadcast('scroll.refreshComplete');
-              $scope.replayAnimation();
-            });
-        }
-        UsersManager.findCerclesByUser(SessionStorage.get(USERFIREBASEPROFILEKEY).uid)
-          .then(function(cerclesRefresh) {
-            allMyCercles = cerclesRefresh;
-          });
-      };
+      //cas des liens non lus
+      if ($state.current.name === 'app.bibli-nonLu') {
+        //Ajout dans biblio
+        liensLus.$add(lien);
+      } else {
+        //Ajout dans non lus
+        liensNonLus.$add(lien);
+      }
+      //Suppression du lien de la liste
+      $scope.deleteLink(lien);
+    };
 
-      //TODO utilisation cordova-plugin-inappbrowser
-      $scope.showURL= function(lien) {
-          window.open(lien.url, '_system', 'location=yes');
-      };
+    $scope.share = function (ev, lien) {
 
-        $scope.canShare= function() {
-            return allMyCercles && allMyCercles[0];
-        };
+      $scope.categories = allCategories;
+      $scope.cercles = allMyCercles;
+      $scope.linkToShare = lien;
 
-        $scope.deleteLink= function(lien) {
-            // $scope.liens est synchronisé avec la base
+      // on trace l'id original
+      var keyOri;
+      if (lien.keyOri) {
+        keyOri = lien.keyOri;
+      } else {
+        keyOri = lien.$id
+      }
+
+      //Initialisation du lien à basculer vers un cercle donné pour une catégorie donnée
+      $scope.shareLink = {
+        title: lien.title,
+        teasing: lien.teasing,
+        createdOn: lien.createdOn,
+        url: lien.url,
+        cercleName: allMyCercles[0].$id,
+        category: allCategories[0],
+        keyOri: keyOri
+      }
+
+      var myPopup = $ionicPopup.show({
+        title: 'Partage',
+        templateUrl: 'templates/el1-share.tpl.html',
+        scope: $scope,
+        buttons: [
+          {text: '<i class="icon ion-close-round"></i>'},
+          {
+            text: '<i class="icon ion-checkmark"></i>',
+            type: 'button-positive',
+            onTap: function (e) {
+              //Lorsque le lien est partagé :
+              //   il est supprimé de read ou notRead
+              //   il est déplacé vers le cercle cible (cercleLinks)
+              //   il est associé à une catégorie (attribut category)
+              return $scope.shareLink;
+            } // onTap
+          }, // button Partager
+        ]
+      });
+
+      myPopup.then(function (shareLink) {
+        //Récupération du lien ajouté
+        GestionService.shareLien(shareLink, SessionStorage.get(USERFIREBASEPROFILEKEY))
+          .then(function () {
             $scope.liens.$remove(lien);
-        };
+            return "Valider";
+          })
+          .catch(function (error) {
+          $log.error(error);
+        })
+      });
 
-        $scope.moveTo = function(lien) {
-            //cas des liens non lus
-            if ($state.current.name === 'app.bibli-nonLu') {
-                //Ajout dans biblio
-                liensLus.$add(lien);
-            } else {
-                //Ajout dans non lus
-                liensNonLus.$add(lien);
-            }
-            //Suppression du lien de la liste
-            $scope.deleteLink(lien);
-        };
+      $timeout(function () {
+        myPopup.close(); //close the popup after 120 seconds
+      }, 120000);
+    }; // fin scope.share
 
-        $scope.share= function(ev, lien) {
-
-            $scope.categories= allCategories;
-            $scope.cercles= allMyCercles;
-            $scope.linkToShare = lien;
-
-            //Initialisation du lien à basculer vers un cercle donné pour une catégorie donnée
-            $scope.shareLink=  {
-              title: lien.title,
-              teasing: lien.teasing,
-              createdOn : lien.createdOn,
-              url : lien.url,
-              cercleName: allMyCercles[0].$id,
-              category: allCategories[0]
-            }
-
-            var myPopup = $ionicPopup.show({
-              title: 'Partage',
-              templateUrl: 'templates/el1-share.tpl.html',
-              scope: $scope,
-              buttons: [
-                { text: '<i class="icon ion-close-round"></i>' },
-                {
-                  text: '<i class="icon ion-checkmark"></i>',
-                  type: 'button-positive',
-                  onTap: function(e) {
-                      //Lorsque le lien est partagé :
-                      //   il est supprimé de read ou notRead
-                      //   il est déplacé vers le cercle cible (cercleLinks)
-                      //   il est associé à une catégorie (attribut category)
-                    return $scope.shareLink;
-                  } // onTap
-                }, // button Partager
-              ]
-            });
-
-            myPopup.then(function(shareLink) {
-              //Récupération du lien ajouté
-              GestionService.shareLien(shareLink, SessionStorage.get(USERFIREBASEPROFILEKEY))
-                .then(function() {
-                  $scope.liens.$remove(lien);
-                  return "Valider";
-                })
-                .catch (function(error) {
-                $log.error(error);
-              })
-            });
-
-            $timeout(function() {
-              myPopup.close(); //close the popup after 120 seconds
-            }, 120000);
-        }; // fin scope.share
-
-    }
+  }
 
 })();
